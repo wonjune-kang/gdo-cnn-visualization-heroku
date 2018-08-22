be821b34faa7e3d44980d61f648166dfc72ca546
Wonjune Kang <wjkang@mit.edu>
2018-08-22 01:49:24 +0100

    Added worker.

--- run-worker.py
+++ run-worker.py
@@ -10,8 +10,8 @@ redis_url = os.getenv('REDISTOGO_URL')
 if not redis_url:
     raise RuntimeError("Set up Redis To Go first.")
 
-urlparse.uses_netloc.append('redis')
-url = urlparse.urlparse(redis_url)
+urllib.parse.uses_netloc.append('redis')
+url = urllib.parse.urlparse(redis_url)
 conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':
